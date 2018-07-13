213d88fb8bc26cafa96f05d51b699fe4378ee80b
PrajwalM2212 <prajwalmmath@gmail.com>
2018-07-13 22:54:32 +0530

    query selector

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ JavaScript can be used to read, write and manupulate DOM
 Object oriented representation
 */
 
+// getElementById() method takes id has input
 /*
 para = document.getElementById('paragraph');
 para.innerText = "Wow java"
@@ -14,8 +15,22 @@ listItem = document.getElementById("list");
 listItem.style.color = 'gray'
 */
 
+// querySelector() method takes element as input 
+
 listItem = document.querySelector('li:last-child');
 listItem.style.color = 'gray';
 unorderedList = document.querySelector('ul');
 unorderedList.style.color = "green";
 console.log(unorderedList);
+
+
+// document.getElementsByClassName()
+
+
+// querySelectorAll()
+/*const list = document.querySelectorAll("div.a  li#li ,  div.a li#l");
+console.log(list)
+arr = Array.from(list);
+arr.forEach(function(e){
+e.style.color = "red";
+});*/
